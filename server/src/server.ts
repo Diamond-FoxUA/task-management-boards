90feb79db1b25faf592158b1a6e15a6f5b8923cd
Dmytro Farbun <dimofarb@gmail.com>
2026-03-24 12:13:41 +0200

    feat(server): add logging middleware with pino, configure cors and helmet

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,18 @@
-import express from "express";
-import "dotenv/config";
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import 'dotenv/config';
+import { logger } from './middleware/logger.js';
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(logger);
+
 const PORT = process.env.PORT ?? 3000;
 
 app.listen(PORT, () => {
-  console.log("Server is running on port: " + PORT);
+  console.log('Server is running on port: ' + PORT);
 });
